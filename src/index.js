49227336304681064f52dcd0757ea915e7026dcc
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-14 16:31:11 -0800

    displayed users one per line

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function User(props) {
 	return(
 		<div className="card">
-			<h5 className="card-header">User {props.id}</h5>
+			<h5 className="card-header">User {props.user.id}</h5>
 			<div className="card-body">
 				<div className="card-text">
-					<img className="float-left img-fluid mr-3 rounded" src={props.avatar} alt="" />
-					<p>{props.firstName} {props.lastName}</p>
+					<img className="float-left img-fluid mr-3 rounded" src={props.user.avatar} alt="" />
+					<p>{props.user.firstName} {props.user.lastName}</p>
 				</div>
 			</div>
 		</div>
@@ -36,6 +36,7 @@ class App extends React.Component {
 		return(
 			<main class="container">
 				<h1>Hi React</h1>
+				{this.state.users.map(user => <User key={user.id} user={user} />)}
 			</main>
 		);
 	}
