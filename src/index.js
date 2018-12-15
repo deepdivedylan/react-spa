44b62ed834bd45219f24878cbb25c96b64fe43f2
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-14 16:12:24 -0800

    added user function component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+function User(props) {
+	return(
+		<div className="card">
+			<h5 className="card-header">User {props.id}</h5>
+			<div className="card-body">
+				<div className="card-text">
+					<img className="float-left img-fluid mr-3 rounded" src={props.avatar} alt="" />
+					<p>{props.firstName} {props.lastName}</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
 class App extends React.Component {
 	render() {
 		return(
